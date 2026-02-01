8706d330bbe7d6cdd1233383fb8872af85f42d39
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 10:30:46 +0600

    added return.js file

---
+++ return.js
@@ -0,0 +1,14 @@
+function tenTimes(num){
+    const result = num * 10;
+    return result;
+}
+
+const output = tenTimes(4);
+console.log(output);
+
+function doubled(number){
+    const double = number * 2;
+    return double;
+}
+const result=doubled(5);
+console.log( 'double holo:',result);
