15470074dea8201f3f95378118dd77e2579755e0
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 09:38:25 +0600

    added parameter.js file

---
+++ parameter.js
@@ -0,0 +1,17 @@
+function square(number){
+    console.log("value of the given number: " , number);
+    const result= number * number;
+    console.log("square of the given number: " , result);
+}
+
+square(5);
+console.log('..............');
+square(45);
+square(756);
+
+function addAll(a,b,c,d){
+    const total= a+b+c+d;
+    console.log('sum is ',total);
+}
+
+addAll(45,67,231,78);
