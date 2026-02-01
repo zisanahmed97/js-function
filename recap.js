1c5c68cc65d07a856403d332f451a2769cf70033
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 10:01:58 +0600

    added recap.js file

---
+++ recap.js
@@ -0,0 +1,9 @@
+function difference(num1 , num2){  // num1,num2 parameter
+    const diff = num1 - num2;
+    console.log(num1,num2, 'difference:', diff);
+}
+
+const fatherAGe= 55;
+const sonAge = 26;
+
+difference(fatherAGe,sonAge);     //value gula argument
