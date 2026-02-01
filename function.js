ad42850b0487063ac0de6555962b17ee11083890
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 09:09:42 +0600

    created js function folder and added function.js file

---
+++ function.js
@@ -0,0 +1,17 @@
+//how to declare
+
+function toFanOff(){
+    console.log('bed theke uth');
+    console.log('switch er shamne ja');
+    console.log('fan ta off kor');
+}
+
+//to call the function
+
+toFanOff();
+console.log('...................');
+console.log('...................');
+console.log('...................');
+console.log('...................');
+
+toFanOff();
