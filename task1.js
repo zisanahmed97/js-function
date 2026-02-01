8b2eaf922ff6f6bc81df80036693bf0c74df4f31
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-01 18:37:42 +0600

    added task1 file

---
+++ task1.js
@@ -0,0 +1,9 @@
+//Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1,num2,num3,num4){
+    const result = num1*num2*num3*num4;
+    return result;
+}
+
+const output = multiply(3,5,6,2);
+console.log('multiply is ',output);
